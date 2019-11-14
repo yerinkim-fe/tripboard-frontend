f01f0785d716664565f50f9c1b978e4a9798024b
yerin <yerin.kim.fe@gmail.com>
2019-11-14 23:44:47 +0900

    FIX config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,8 @@
 let SERVER_URL;
-
-if (process.env.NODE_ENV === 'development') {
+if (process.env.REACT_APP_NODE_ENV === 'development') {
   SERVER_URL = 'http://localhost:4000';
-} else if (process.env.NODE_ENV === 'production') {
-  SERVER_URL = 'null';
+} else {
+  SERVER_URL = 'http://tripboardbackend.nr3apggekj.ap-northeast-2.elasticbeanstalk.com';
 }
 
 export const config = {
