24d33343662bb5e33201fc0bad3c312f46b227ed
yerin <yerin.kim.fe@gmail.com>
2019-11-15 14:05:29 +0900

    FIX server url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ let SERVER_URL;
 if (process.env.REACT_APP_NODE_ENV === 'development') {
   SERVER_URL = 'http://localhost:4000';
 } else {
-  SERVER_URL = 'http://tripboardapi.yerinsite.com';
+  SERVER_URL = 'https://tripboardapi.yerinsite.com';
 }
 
 export const config = {
