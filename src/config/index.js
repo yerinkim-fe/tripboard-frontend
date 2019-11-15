c1a1eabc2f4c7d3e259907cd958e86c9910d2b33
yerin <yerin.kim.fe@gmail.com>
2019-11-15 13:11:04 +0900

    FIX server url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ let SERVER_URL;
 if (process.env.REACT_APP_NODE_ENV === 'development') {
   SERVER_URL = 'http://localhost:4000';
 } else {
-  SERVER_URL = 'https://tripboardbackend.nr3apggekj.ap-northeast-2.elasticbeanstalk.com';
+  SERVER_URL = 'https://tripboardapi.yerinsite.com';
 }
 
 export const config = {
